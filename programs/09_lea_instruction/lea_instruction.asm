868405d8b653a201acb2080ef316f43772985e1b
Kosta Bulgakov <kosta.bulgakov@mail.huji.ac.il>
2023-11-27 16:27:43 +0200

    wip

---
+++ programs/09_lea_instruction/lea_instruction.asm
@@ -0,0 +1,18 @@
+section .data
+var dq 10
+
+section .text
+global main
+
+main:
+
+; LEA - load effective address
+    
+    lea rax, [var]              ; Now rax has address of var
+    mov rbx, qword[rax]         ; RBX <- var
+    
+    ; lea allows arithmetic operations of the address
+    lea rdx,[rax+10]
+
+    xor rax, rax
+    ret
